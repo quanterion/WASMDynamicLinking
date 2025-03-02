4d88455fbf10e615f6985cff854c5b1577e4d23c
DESKTOP-QUINTEN\quint <quinten.vanhooydonck@hotmail.com>
2025-03-02 22:03:59 +0100

    Add comments to build files and source code

--- CompileToWasm.sh
+++ CompileToWasm.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+# Set as side module, need exactly 1 main module, the other modules should be side modules
+# Export all functions, this epxorts all functions, classes, structs and enums. You can use EMSCRIPTEN_KEEPALIVE instead, but this way of exporting works different from exporting for other platforms.
+# -sPROXY_TO_PTHREAD -pthread is required to allow dlopen to load a library. Otherwise the library has to be auto loaded
+# force shared memory on, this is required so the executable and library can share data
+# Wno-experimental disables a warning related to pthreads
 emcc -sSIDE_MODULE=1 -sMAIN_MODULE=0 -s EXPORT_ALL=1 -sPROXY_TO_PTHREAD -pthread -sSHARED_MEMORY=1 -Wno-experimental -o Build/libcalc.so Src/calc.cpp
+
+# set as main module, this is required to allow to the linker to find the symbols in a library
+# Disable auto loading, this is disabled to allow dynamically loading libraries, if you want to load the libraries while the page is loading, enable auto load
+# force shared memory on, this is required so the executable and library can share data
+# Wno-experimental disables a warning related to pthreads
 emcc -sMAIN_MODULE=2 -sAUTOLOAD_DYLIBS=0 -sPROXY_TO_PTHREAD -pthread -sSHARED_MEMORY=1 -Wno-experimental -o Build/main.html Build/libcalc.so Src/main.cpp
--- Src/main.cpp
+++ Src/main.cpp
@@ -6,6 +6,8 @@ int main()
 {
 	std::cout << "Loading wasm lib" << '\n';
 	
+	// Wasm uses either wasm or so files for library, loading an so file here
+	// RTLD_GLOBAL is required here, otherwise the library won't be accessible by linked functions after loading
 	void* dllHandle = dlopen("libcalc.so", RTLD_NOW|RTLD_GLOBAL);
 	if(!dllHandle)
 	{
@@ -15,6 +17,9 @@ int main()
 	{
 		std::cout << "Handle: " << int(dllHandle) << '\n';
 	}
+	
+	// Finds the add symbol and prints if it was found\n
+	// This name could be mangled in a different way, so might not be able to find the symbol even when it exists
 	void* symbol = dlsym(dllHandle, "_ZN4Calc3addEii");
     if (symbol) {
         std::cout << "Found Symbol: " << "_ZN4Calc3addEii" << '\n';
@@ -22,7 +27,7 @@ int main()
         printf("Symbol not found\n");
     }
 	
-	std::cout << "hello world" << '\n';
+	std::cout << "Finished loading, testing libraries!" << '\n';
 	std::cout << "Add 3 + 5 = " << Calc::add(3, 5) << '\n';
 	std::cout << "subtract 5 - 3 = " << Calc::sub(5, 3) << '\n';
 }
